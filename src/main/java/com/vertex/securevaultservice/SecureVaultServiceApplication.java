6497413ebbf05b412c5e0eed620fda8fcbbf22ff
Sai-Chakradhar-Mahendrakar <saichakradharr@gmail.com>
2026-09-22 20:28:39 +0530

    [SV] Setup

---
+++ src/main/java/com/vertex/securevaultservice/SecureVaultServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vertex.securevaultservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureVaultServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureVaultServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vertex/securevaultservice/SecureVaultServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vertex.securevaultservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureVaultServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
